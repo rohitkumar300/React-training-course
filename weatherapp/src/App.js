878a041d98860c7231ce12e36490cac4ab126524
rohitkumar300 <rohit@ikshalabs.com>
2022-10-13 14:27:39 +0530

    api fetched

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
+
+const Display = () =>{
+  return (
+    <div>
+      This is Display area
+    </div>
+  )
+}
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const [search, Setsearch] = useState();
+
+  useEffect(() => {
+    // const API_LINK = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${API_KEY}`
+    const API_LINK = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=63d6d88996e578187824190c05551ea7`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_LINK);
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, [search]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    Setsearch(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is weather App
+      <input type="text" name="search" onChange={handleChange} value={search} />
+      <Display />
     </div>
   );
 }
