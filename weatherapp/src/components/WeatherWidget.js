180ff698f0dbfe1bf9247b744d3d27c5fefad4a2
rohitkumar300 <rohit@ikshalabs.com>
2022-10-14 12:37:40 +0530

    Improved ui and fixed issue for invalid cities

--- weatherapp/src/components/WeatherWidget.js
+++ weatherapp/src/components/WeatherWidget.js
@@ -1,28 +1,41 @@
 import React, { useState,useEffect } from "react";
 export const WeatherWidget = (props) => {
     const [city, SetCity] = useState("");
+    const [longitude, SetLongitude] = useState(0);
+    const [latitude, SetLatitude] = useState(0);
     const [country, SetCountry] = useState("");
     const [temperature, SetTemperature] = useState(0);
     const [pressure, SetPressure] = useState(0);
+    const [humidity, SetHumidity] = useState(0);
+    const [windspeed,SetWindspeed] = useState(0);
+    const [description, SetDescription] = useState("");
   
     useEffect(()=>{
       SetCity(props.city);
+      SetLongitude(props.longitude);
+      SetLatitude(props.latitude)
       SetCountry(props.country);
       SetTemperature(props.temperature);
       SetPressure(props.pressure);
+      SetHumidity(props.humidity);
+      SetWindspeed(props.windspeed);
+      SetDescription(props.description);
   
     },[props.city]);
   
     return (
       <div>
-        <h1>City : </h1>
-        <h3> {city} </h3>
-        <h1>Country : </h1>
-        <h3> {country} </h3>
-        <h1>Temperature : </h1>
-        <h3>{temperature} </h3>
-        <h1>Pressure : </h1>
-        <h3>{pressure} </h3>
+
+        <h6>{city}</h6>
+        <h6>{longitude}</h6>
+        <h6>{latitude}</h6>
+        <h6>{country}</h6>
+        <h6>{temperature}</h6>
+        <h6>{pressure}</h6>
+        <h6>{humidity}</h6>
+        <h6>{windspeed}</h6>
+        <h6>{description}</h6>
+        
       </div>
     );
   };
